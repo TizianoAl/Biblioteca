69d6fa3868dd03d8ed538c8cc6c66174f7c8d531
TizianoAl <alfazaktiziano@gmail.com>
2020-08-19 19:38:15 -0300

    Inlines hechos

--- Proyecto 3/bilioteca/admin.py
+++ Proyecto 3/bilioteca/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from .models import *
 
+class MaterialInline(admin.TabularInline):
+    model = Material
+    fields = ['tipoMaterial','autor','titulo','anio','status']
+
+class PersonaInline(admin.TabularInline):
+    model = Persona
+    fields = ['tipoPersona','nombre','apellido','correo','telefono','numLibros','adeudo']
+
+class PrestamoAdmin(admin.ModelAdmin):
+    inlines = [MaterialInline, PersonaInline]
+
+
+
+
+
 # Register your models here.
 
-admin.site.register(Prestamo)
-admin.site.register(Material)
+admin.site.register(Prestamo, PrestamoAdmin)
 admin.site.register(Persona)
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Material)
 admin.site.register(Libro)
 admin.site.register(Revista)
-admin.site.register(Alumno)
-admin.site.register(Profesor)
