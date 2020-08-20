675a911bbfba32e17d637c58917d47d55bb28d2f
TizianoAl <alfazaktiziano@gmail.com>
2020-08-19 22:25:39 -0300

    Biblioteca Terminada

--- Proyecto 3/config/settings.py
+++ Proyecto 3/config/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+        'DIRS': [os.path.normpath(os.path.join(BASE_DIR, 'templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 JET_THEMES = [
     {
